13cc3f916507f841cbf9c28399b7602bd5d97906
hheydarian <hamdhydryan12@gmail.com>
2025-04-07 20:31:28 +0330

    Initial commit - added TextUtilities library

---
+++ TextUtilities/TextHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace TextUtilities;
+
+public class TextHelper
+{
+    //Remove extra spaces from text
+    public static string NormalizeSpaces(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return string.Empty;
+
+        return string.Join(" ", input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+    }
+
+    // Convert the first letter of each word to uppercase.
+    public static string ToTitleCase(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return string.Empty;
+
+        return System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(input.ToLower());
+    }
+
+    // Checking whether a string contains only numbers
+    public static bool IsNumeric(string input)
+    {
+        return double.TryParse(input, out _);
+    }
+}
---
+++ TextUtilityTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using TextUtilities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== تست متد NormalizeSpaces ===");
+        string messyText = " This   is   a   test   text.";
+        string cleanText = TextHelper.NormalizeSpaces(messyText);
+        Console.WriteLine(cleanText);
+
+        Console.WriteLine("\n=== test method ToTitleCase ===");
+        string lower = "salam hamed";
+        string titled = TextHelper.ToTitleCase(lower);
+        Console.WriteLine(titled);
+
+        Console.WriteLine("\n=== test method IsNumeric ===");
+        Console.WriteLine(TextHelper.IsNumeric("123"));
+        Console.WriteLine(TextHelper.IsNumeric("hamed"));
+    }
+}
